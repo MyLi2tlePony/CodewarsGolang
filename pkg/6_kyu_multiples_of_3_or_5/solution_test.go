291e03ee200891f54a05d56abbf2192d6dce5719
MyLi2tlePony <shabandrew@mail.ru>
2022-11-05 18:51:51 +0300

    multiples_of_3_or_5 is completed

---
+++ pkg/6_kyu_multiples_of_3_or_5/solution_test.go
@@ -0,0 +1,12 @@
+package multiples_of_3_or_5
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	t.Run("tests", func(t *testing.T) {
+		require.Equal(t, solution(10), 23)
+	})
+}
